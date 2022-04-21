b050c73e0d154cfc8c69116965102201dbe01820
Kyle Falk <kyle.falk93@gmail.com>
2022-04-20 21:36:44 -0400

    final tutorial

--- src/arena_capture_the_flag/main.ts
+++ src/arena_capture_the_flag/main.ts
@@ -1,33 +1,3 @@
-// Note that there is no global objects like Game or Memory. All methods, prototypes and constants are imported built-in modules
-// import {
-//   ATTACK,
-//   CostMatrix,
-//   HEAL,
-//   RANGED_ATTACK,
-//   RoomPosition,
-//   getDirection,
-//   getRange,
-//   getObjectById,
-//   getObjectsByPrototype,
-//   getTime
-// } from "game";
-
-// Everything can be imported either from the root /game module or corresponding submodules
-// import { pathFinder } from "game";
-// pathFinder.searchPath();
-// import { prototypes } from "game";
-// prototypes.Creep
-// prototypes.RoomObject
-
-// import {searchPath } from '/game/path-finder';
-// import {Creep} from '/game/prototypes';
-
-// This would work too:
-// import * as PathFinder from '/game/path-finder'; --> PathFinder.searchPath
-// import {Creep} from '/game/prototypes/creep';
-// import * as prototypes from '/game/prototypes'; --> prototypes.Creep
-
-// This stuff is arena-specific
 import { ATTACK, HEAL, RANGED_ATTACK } from "game/constants";
 import { Creep, GameObject } from "game/prototypes";
 import { getDirection, getObjectsByPrototype, getRange, getTicks } from "game/utils";
@@ -41,17 +11,10 @@ declare module "game/prototypes" {
   }
 }
 
-// You can also import your files like this:
-// import {roleAttacker} from './roles/attacker.mjs';
-
-// We can define global objects that will be valid for the entire match.
-// The game guarantees there will be no global reset during the match.
-// Note that you cannot assign any game objects here, since they are populated on the first tick, not when the script is initialized.
 let myCreeps: Creep[];
 let enemyCreeps: Creep[];
 let enemyFlag: Flag | undefined;
 
-// This is the only exported function from the main module. It is called every tick.
 export function loop(): void {
   // We assign global variables here. They will be accessible throughout the tick, and even on the following ticks too.
   // getObjectsByPrototype function is the alternative to Room.find from Screeps World.
@@ -59,11 +22,6 @@ export function loop(): void {
   myCreeps = getObjectsByPrototype(Creep).filter(i => i.my);
   enemyCreeps = getObjectsByPrototype(Creep).filter(i => !i.my);
   enemyFlag = getObjectsByPrototype(Flag).find(i => !i.my);
-
-  // Notice how getTime is a global function, but not Game.time anymore
-  if (getTicks() % 10 === 0) {
-    console.log(`I have ${myCreeps.length} creeps`);
-  }
 
   // Run all my creeps according to their bodies
   myCreeps.forEach(creep => {
--- src/arena_tutorial_final_test/main.ts
+++ src/arena_tutorial_final_test/main.ts
@@ -3,31 +3,67 @@ import { getObjectsByPrototype, getTicks } from "game/utils";
 
 import { hello } from "common/hello";
 import { isFirstTick } from "common";
-import {Creep, StructureSpawn} from "game/prototypes";
-import {ATTACK, MOVE, WORK} from "game/constants";
+import {Creep, StructureSpawn, Source} from "game/prototypes";
+import {ATTACK, CARRY, ERR_NOT_IN_RANGE, HEAL, MOVE, RANGED_ATTACK, RESOURCE_ENERGY, WORK} from "game/constants";
 
 let myCreeps: Creep[];
 let enemyCreeps: Creep[];
+let mySpawn: StructureSpawn;
+let workerCreepCount = 0;
+let attackerCreepCount = 0;
+let source: Source;
 
 export function loop(): void {
   if (isFirstTick()) {
     console.log(hello());
   }
 
+
+  source = getObjectsByPrototype(Source)[0];
   myCreeps = getObjectsByPrototype(Creep).filter(i => i.my);
   enemyCreeps = getObjectsByPrototype(Creep).filter(i => !i.my);
+  mySpawn = getObjectsByPrototype(StructureSpawn)[0];
 
-  // Notice how getTime is a global function, but not Game.time anymore
-  if (getTicks() % 10 === 0) {
-    console.log(`I have ${myCreeps.length} creeps`);
+  let spawnEnergy = mySpawn.store[RESOURCE_ENERGY];
+
+  if (workerCreepCount == 0) {
+   mySpawn.spawnCreep([MOVE, WORK, CARRY]).object;
+   workerCreepCount ++;
+  } else {
+    if (spawnEnergy >= 130) {
+      mySpawn.spawnCreep([ATTACK, MOVE]).object;
+      attackerCreepCount++;
+    }
   }
 
-  var mySpawn = getObjectsByPrototype(StructureSpawn)[0];
+  // Run all my creeps according to their bodies
+  myCreeps.forEach(creep => {
 
-  if (myCreeps.length === 0) {
-   var creep = mySpawn.spawnCreep([MOVE, WORK]).object;
+    if (creep.body.some(i => i.type == WORK)) {
+       workerCreep(creep);
+    }
+
+    if (creep.body.some(i => i.type == ATTACK)) {
+      attackerCreep(creep);
+    }
+  });
+}
+
+function attackerCreep(creep: Creep) {
+  if (creep.attack(enemyCreeps[0]) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(enemyCreeps[0]);
   }
+}
 
-
-
+function workerCreep(creep: Creep) {
+  let energyCapacity = creep.store[RESOURCE_ENERGY];
+  if (energyCapacity < 50) {
+    if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(source);
+      }
+  } else {
+    if (creep.transfer(mySpawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(mySpawn);
+    }
+  }
 }
